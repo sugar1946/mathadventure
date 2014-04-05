038e44df69bc113be84d1bd268df9b2259a3a9b6
bojangles495 <robert@isminc.com>
2014-04-05 10:49:08 -0400

    testing the repo

---
+++ THE_GAME/game/Main.py
@@ -0,0 +1,11 @@
+#Robert Jones 
+#CISC 374
+import spyral
+import random
+import math
+
+
+class Main(spyral.Scene):
+
+	def __init__(self, *args, **kwargs):
+		print "got here"
---
+++ THE_GAME/game/__init__.py
@@ -0,0 +1,6 @@
+import spyral
+import Main
+
+def main():
+	spyral.director.push(Main.Main())
+	print "got hrer"
