3684d67b6649ffd075ab01ba99726abdf1f24569
bojangles495 <robert@isminc.com>
2014-04-08 11:15:27 -0400

    Just testing things

--- THE_GAME/game/__init__.py
+++ THE_GAME/game/__init__.py
@@ -2,4 +2,8 @@ import spyral
 import Main
 
 def main():
+	spyral.director.init(size=(0, 0), max_ups=30, max_fps=30, fullscreen=False, caption='The Game')
 	spyral.director.push(Main.Main())
+	spyral.director.push(Board.Board())
+	#spyral.event.register("input.keyboard.down.r", spyral.director.push(self.scene2))
+	#self.sene2 = spyral.Scene.__init__(self,SIZE2)
