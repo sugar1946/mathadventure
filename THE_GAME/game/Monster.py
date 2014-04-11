06f85668df68d87e292eb7f8eae9a61d76df2cb3
sugar1946 <mingzhi@udel.edu>
2014-04-11 10:38:47 -0400

    Monster class
    So i added a monster class but I have seen some one added an opponent
    class in character which is similar to mine. Thank for the people who
    is generous doing my work.I did not add monster class into the main fun
    on main.py  in case  monster crashes the whole program, Therefore you
    could not see the monster on the screen for now. The program works fine
    on my computer now  I will keep on working on this. BTW, bmp files have
    been converted into png file on MAC, which does not work on pygame.
    Thus, I added several bmp file into image folder. But I did not do
    anything to __init__.py

---
+++ THE_GAME/game/Monster.py
@@ -0,0 +1,49 @@
+import spyral
+import random
+import math
+WIDTH = 900
+HEIGHT = 900
+BG_COLOR = (255,255,255)
+BLACK = (0, 0, 255)
+SIZE = (WIDTH, HEIGHT)
+MONSTER.IMG = "game/images/m1.bmp"
+
+class Monster(spyral.Sprite):
+    def __inti__(self,scene):
+        self.image = spyral.Image(filename = MONSTER.IMG)
+        self.anchor = "center"
+        self.x = random.random()*WIDTH
+        self.y = random.random()*HEIGHT
+        self.moving = False
+        self.vel_x  = 50
+        self.vel_y = 50
+        spyral.event register('director.update',self.update)
+
+    def update(self,delta):
+        self.x += delta * self.vel_x
+        self.y += delta * self.vel_y
+        chance = random.random();
+
+        ## bounce
+        r = self.rect            
+        if r.top < 0:
+            r.top = 0
+            self.vel_y = -self.vel_y
+        if r.bottom > HEIGHT:
+            r.bottom = HEIGHT
+            self.vel_y = -self.vel_y
+        if r.left < 0:
+            r.left = 0
+            self.vel_x = -self.vel_x
+        if r.right > WIDTH:
+            self.vel_x = -self.vel_x
+        ##change the direction , during the move, the monster would change its direction by 30% possibility
+        if(chance=<0.3):
+        ## pick up a random angle for monster to move
+            theta = random.random()*2*math.pi
+            r = 300
+            self.vel_x = r*math.cos(theta)
+            self.vel_y = r*math.sin(theta)
+
+                
+    
