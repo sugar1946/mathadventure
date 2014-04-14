f42f70de274b3357509971518385874bcff2d675
sugar1946 <mingzhi@udel.edu>
2014-04-13 20:03:31 -0400

    so basically i make the monster class similar to the character
    did not do much modification to the main fun . The game works on my
    computer

--- THE_GAME/game/Board.py
+++ THE_GAME/game/Board.py
@@ -3,6 +3,7 @@ import random
 import math
 import Walls
 import Character
+import Monster
 WIDTH = 600
 HEIGHT = 600
 BG_COLOR = (255,255,255)
@@ -10,27 +11,39 @@ BLACK = (0, 0, 255)
 SIZE = (WIDTH, HEIGHT)
 SIZE2 = (WIDTH/2, HEIGHT/2)
 WALL_LIST = []
+ENEMY_LIST = []
 class Board(spyral.Scene):
-	text = '';
-	def __init__(self, *args, **kwargs):
-	
-		spyral.Scene.__init__(self, SIZE)
-		spyral.event.register("system.quit", spyral.director.pop) 
-		spyral.event.register("input.keyboard.down.q", spyral.director.pop)
-		spyral.event.register("director.update", self.update)
+    text = ''
+    ENEMY_LIST = []
+    def __init__(self, *args, **kwargs):
+        spyral.Scene.__init__(self, SIZE)
+        # self.monster = Monster.Monster(self)
+        spyral.event.register("system.quit", spyral.director.pop)
+        spyral.event.register("input.keyboard.down.q", spyral.director.pop)
+        spyral.event.register('director.update', self.update)
+        
 
-	def update(self,delta):
-		for wall in WALL_LIST:
-			self.player.collide_wall(wall)
-	
-	def setCharacter(self,character):
+    def update(self,delta):
+        for wall in WALL_LIST:
+            self.player.collide_wall(wall)
+            for enemy in ENEMY_LIST:
+                enemy.collide_wall(wall)
+                enemy.collide_player(self.player)
+                    
+
+    def setCharacter(self,character):
 		self.player = character
-		character.setKeyBoardCommands(self)	
+		character.setKeyBoardCommands(self)
+
+    def setMonster(self,monsters):
+        for i in range(len(monsters)):
+            ENEMY_LIST.append(monsters[i])
+            monsters[i].setUpdate(self)
 	
-	def setBackGround(self,imagePath):
+    def setBackGround(self,imagePath):
 		self.background = spyral.Image(filename=imagePath)
 
-	def setWalls(self,quadrantRow,quadrantColumn):
+    def setWalls(self,quadrantRow,quadrantColumn):
 		if(quadrantRow == 0 and quadrantColumn == 0):
 			wall = Walls.Walls(self)
 			wall.wallTopHalf('Left')
--- THE_GAME/game/Monster.py
+++ THE_GAME/game/Monster.py
@@ -1,49 +1,79 @@
 import spyral
 import random
 import math
+
 WIDTH = 900
 HEIGHT = 900
 BG_COLOR = (255,255,255)
 BLACK = (0, 0, 255)
 SIZE = (WIDTH, HEIGHT)
-MONSTER.IMG = "game/images/m1.bmp"
+SIZE2 = (WIDTH/2, HEIGHT/2)
+MONSTER = "game/images/m1.bmp"
+
 
 class Monster(spyral.Sprite):
-    def __init__(self,scene):
-        self.image = spyral.Image(filename = MONSTER.IMG)
-        self.anchor = "center"
-        self.x = random.random()*WIDTH
-        self.y = random.random()*HEIGHT
-        self.moving = False
-        self.vel_x  = 50
-        self.vel_y = 50
-        spyral.event register('director.update',self.update)
+    def __init__(self):
+        #super(Character, self).__init__(scene)
+        score = 0
+        
+        
 
+    def setScene(self,scene):
+        super(Monster,self).__init__(scene)
+
+    def setImage(self,imagePath):
+    #"game/images/stick.png"
+        self.image = spyral.Image(filename=imagePath)
+        self.anchor = "center"
+        self.x=100*random.random()
+        self.y=100*random.random()
+        self.moving = False
+        self.vel_x = 20
+        self.vel_y = 20
+        
     def update(self,delta):
         self.x += delta * self.vel_x
         self.y += delta * self.vel_y
         chance = random.random();
 
         ## bounce
-        r = self.rect            
-        if r.top < 0:
-            r.top = 0
+        r = self.rect
+        if r.top < 30:
+            r.top = 30
             self.vel_y = -self.vel_y
-        if r.bottom > HEIGHT:
-            r.bottom = HEIGHT
+        if r.bottom > HEIGHT-30:
+            r.bottom = HEIGHT-30
             self.vel_y = -self.vel_y
-        if r.left < 0:
-            r.left = 0
+        if r.left < 30:
+            r.left = 30
             self.vel_x = -self.vel_x
-        if r.right > WIDTH:
+        if r.right > WIDTH-30:
             self.vel_x = -self.vel_x
         ##change the direction , during the move, the monster would change its direction by 30% possibility
-        if(chance=<0.3):
+        if(chance<=0.3):
         ## pick up a random angle for monster to move
             theta = random.random()*2*math.pi
-            r = 300
+            r = 100
             self.vel_x = r*math.cos(theta)
             self.vel_y = r*math.sin(theta)
 
-                
-    
+    def setUpdate(self,scene):
+        spyral.event.register('director.update', self.update)
+
+    def collide_wall(self,wall):
+        if self.collide_sprite(wall):
+            self.vel_x = -self.vel_x
+            self.vel_y = -self.vel_y
+            self.moving = False
+        
+    def collide_monster(self,monster):
+        if self.collide_sprite(monster):
+            self.vel_x = -self.vel_x
+            self.vel_y = -self.vel_y
+
+    def collide_player(self,character):
+        if self.collide_sprite(character):
+           self.vel_x = -self.vel_x
+           self.vel_y = -self.vel_y
+
+
