0cd9f012e6643a3aff9178be7e40e700e47b3e1e
blutuu <blutuu@udel.edu>
2014-04-16 19:17:42 -0400

    No change

---
+++ THE_GAME/game/Item.py
@@ -0,0 +1,29 @@
+import spyral
+import random
+import Question
+
+class Item(spyral.Sprite):
+    def __init__(self, scene, name, x, y):
+        super(Item, self).__init__(scene)
+        question_level = "easy"
+        attempts = 0
+        self.name = name
+        self.key = False
+
+        if (self.name == "chest"):
+            self.image = spyral.Image(filename=("game/images/chest.bmp"))
+            self.image.scale((90,65))
+        elif (self.name == "gem"):
+            self.image = spyral.Image(filename=("game/images/gem.png"))
+            self.image.scale((40,60))
+
+        self.anchor = "bottomright"
+        self.x = x
+        self.y = y
+
+        
+        spyral.event.register("collision", self.collision)
+        
+    def collision(self):
+        return 0
+
