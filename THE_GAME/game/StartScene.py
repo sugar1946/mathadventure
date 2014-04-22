e627f2be48c81414c5a76d7d0f77b0f38348f999
merrinmeltzer <mmeltzer@udel.edu>
2014-04-21 20:14:37 -0400

    minor change

--- THE_GAME/game/StartScene.py
+++ THE_GAME/game/StartScene.py
@@ -6,7 +6,7 @@ class StartScene(spyral.Scene):
 			spyral.Scene.__init__(self, SIZE)
 			image = spyral.Image(filename="game/sceneImages/start.bmp")
 			image.scale((1200,900))
-			self.background = spyral.Image(filename="game/sceneImages/loading.bmp")
+			self.background = image
 			spyral.event.register("input.keyboard.down.q", spyral.director.pop)
 			spyral.event.register("input.keyboard.down.t", spyral.director.pop)
 			spyral.event.register("input.keyboard.down.s", self.startGame)
